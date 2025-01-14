9a068c6809023a991655bfcc7b8ebb014b4359ce
evanalifian <evanalifian@gmal.com>
2025-01-15 00:27:32 +0700

    create mdx component

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import Home from "./pages/Home";
+import Example from "./pages/Example.mdx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/mdx-example" element={<Example />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
