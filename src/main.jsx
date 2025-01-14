73821b40206b96782e544e8dd126614ad65d3268
evanalifian <evanalifian@gmal.com>
2025-01-14 18:14:22 +0700

    delete users element

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.jsx";
+import Home from "./pages/Home";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
