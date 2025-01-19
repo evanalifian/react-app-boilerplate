a92675829c401b07a95820070bbb3dbc2e1c6a75
evanalifian <evanalifian@gmal.com>
2025-01-19 19:50:47 +0700

    modified: change importing element and update route path

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import Home from "./pages/Home";
-import Example from "./pages/Example.mdx";
+import Home from "./pages/home/Page";
+import Page from "./pages/mdx-example/Page";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mdx-example" element={<Example />} />
+        <Route path="/mdx-example" element={<Page />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
